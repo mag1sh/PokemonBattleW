332d507b270aa70a268e027550a426901e2da036
User <User@DESKTOP-VGLENU1>
2025-07-03 14:48:15 +0300

    Add project files.

---
+++ Pokemon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonBattleW
+{
+    internal class Pokemon
+    {
+        public int ID;
+        public string Name;
+        public int Attack;
+        public int Defence;
+        public int Strength;
+        public int Health;
+        public int Owner;
+        public bool IsAlive;
+
+        public Pokemon(int id, string name, int attack, int defence, int strength, int health, int owner, bool isAlive)
+        {
+            ID = id;
+            Name = name;
+            Attack = attack;
+            Defence = defence;
+            Strength = strength;
+            Health = health;
+            Owner = owner;
+            IsAlive = isAlive;
+        }
+    }
+}
